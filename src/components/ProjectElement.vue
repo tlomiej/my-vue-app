de3b6ad85ed179f92b292b5467c32ac4c80a1105
Bartłomiej Staroń <bartekstaron@gmail.com>
2022-06-16 23:43:10 +0200

    proj4

--- src/components/ProjectElement.vue
+++ src/components/ProjectElement.vue
@@ -16,13 +16,20 @@ export default {
     showCoords: () => {
       console.log("cords");
 
-      const firstProjection =
-        'PROJCS["NAD83 / Massachusetts Mainland",GEOGCS["NAD83",DATUM["North_American_Datum_1983",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],AUTHORITY["EPSG","6269"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.01745329251994328,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4269"]],UNIT["metre",1,AUTHORITY["EPSG","9001"]],PROJECTION["Lambert_Conformal_Conic_2SP"],PARAMETER["standard_parallel_1",42.68333333333333],PARAMETER["standard_parallel_2",41.71666666666667],PARAMETER["latitude_of_origin",41],PARAMETER["central_meridian",-71.5],PARAMETER["false_easting",200000],PARAMETER["false_northing",750000],AUTHORITY["EPSG","26986"],AXIS["X",EAST],AXIS["Y",NORTH]]';
-      const secondProjection =
-        "+proj=gnom +lat_0=90 +lon_0=0 +x_0=6300000 +y_0=6300000 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+      Proj4.defs([
+        [
+          "EPSG:4326",
+          "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees",
+        ],
+        [
+          "EPSG:4269",
+          "+title=NAD83 (long/lat) +proj=longlat +a=6378137.0 +b=6356752.31414036 +ellps=GRS80 +datum=NAD83 +units=degrees",
+        ],
+      ]);
+
       //I'm not going to redefine those two in latter examples.
-      const project = Proj4(firstProjection, secondProjection, [2, 5]);
-      console.log(project, '<---');
+      const project = Proj4("EPSG:4326", "EPSG:4269", [2, 5]);
+      console.log(project, "<---");
     },
   },
 };
